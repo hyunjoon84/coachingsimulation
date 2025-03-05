8201fcda13aa2fb0068e72e615a7cd1790a0ff0d
Hyunjoon Um <hjum84@gmail.com>
2025-03-05 18:39:50 -0500

    app.py updated

--- app.py
+++ app.py
@@ -812,4 +812,5 @@ def get_session_details(session_id):
 
 if __name__ == "__main__":
     os.makedirs("static/audio", exist_ok=True)
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", debug=True, port=port)
